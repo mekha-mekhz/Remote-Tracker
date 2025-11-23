66327d4d636213db66546189d49f0a377fe7e28a
Mekha <mohanananmekha04@gmail.com>
2025-11-23 19:36:30 +0530

    routes updated

--- SERVER/index.js
+++ SERVER/index.js
@@ -11,6 +11,9 @@ const connectdb = require("./config/db");
 const authuser = require("./routes/authroutes");
 const taskRoutes=require("./routes/taskroutes")
 const timeroutes=require("./routes/timeroutes")
+const attendanceroutes=require("./routes/attendanceroutes")
+const leaveroutes=require("./routes/leaveroutes")
+const productivityroutes=require("./routes/dailreprtroutes")
 
 connectdb()
 var cors = require("cors");
@@ -27,6 +30,9 @@ res.send("WELCOME TO REMOTE WORK TRACKER ")
 app.use("/api", authuser);
 app.use("/api/tasks", taskRoutes)
 app.use("/api/time",timeroutes)
+app.use("/api/attendance",attendanceroutes)
+app.use("/api/leave",leaveroutes)
+app.use("/api/productivity",productivityroutes)
 
 
 app.listen(process.env.PORT,()=>{
